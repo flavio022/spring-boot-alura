2a6bcd00402baa61c2c325a41fc3f21a78d0362d
Flavio <flavio.danilo022@gmail.com>
2020-12-15 20:17:16 -0300

    Delet topico

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,7 @@ public class TopicoController {
 
 	@PostMapping
 	@ResponseBody
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -70,6 +72,16 @@ public class TopicoController {
 		Topico topico = form.atualizar(id, topicoRepository);
 		
 		return ResponseEntity.ok(new TopicoDto(topico));
+		
+	}
+	
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity remover(@PathVariable Long id) {
+		topicoRepository.deleteById(id);
+		
+		return ResponseEntity.ok().build();
 		
 	}
 
